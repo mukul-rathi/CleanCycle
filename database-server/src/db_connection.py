4ab56112e34c0774db6b2b1a75c0b02b6097b6cb
Mukul Rathi <mukul.rathi@gmail.com>
2019-02-18 00:04:42 +0000

    Add method to insert csv data into database

--- database-server/src/db_connection.py
+++ database-server/src/db_connection.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2 import sql
+from sqlalchemy import create_engine
 import json
 import os 
 
@@ -24,12 +25,14 @@ schemas = {
     "air_quality" : "(uuid TEXT PRIMARY KEY, Latitude FLOAT8,  Longitude FLOAT8, PM10 FLOAT8, PM25 FLOAT8)"
 }
 class DBConnection:
-    def __init__(self):
+         
+  def __init__(self, dbname=os.environ['POSTGRES_DB'], dbuser=os.environ['POSTGRES_USER'], dbpassword= os.environ['POSTGRES_PASSWORD'], hostname="host.docker.internal", port=5432):
       #set up connection
-      connectionParameters = "dbname=" + os.environ['POSTGRES_DB']
-      connectionParameters += " user=" + os.environ['POSTGRES_USER']
-      connectionParameters += " password=" + os.environ['POSTGRES_PASSWORD']
-      connectionParameters += " host=host.docker.internal port=5432"
+    engineParams = f"postgresql+psycopg2://{dbuser}:{dbpassword}@{hostname}:{port}/{dbname}"
+    self._engine = create_engine(engineParams)
+    self._conn = self._engine.raw_connection()
+    self._cur = self._conn.cursor()
+
      
       self._conn = psycopg2.connect(connectionParameters)
       self._cur = self._conn.cursor()
@@ -38,9 +41,16 @@ class DBConnection:
       self._cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
       self._conn.commit()
 
-
-    def insertItem(self):
-      self._cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+  def insertSensorData(self, csvFile):
+    data = pd.read_csv(csvFile)
+    #generate unique uid for each measurement
+    data["uuid"] = [uuid.uuid4().hex for _ in range(data.shape[0])]
+    for table in columns.keys():
+      table_df = data[columns[table]]
+      output = io.StringIO()
+      table_df.to_csv(output, sep='\t', header=False, index=False)
+      output.seek(0)
+      self._cur.copy_from(output, table, null="") 
       self._conn.commit()
 
 
