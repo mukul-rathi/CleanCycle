88a962a152348afc4ccafd3d912da97df79a004d
M. J. W-H. Campman <mjwhc2@cam.ac.uk>
2019-02-21 10:13:49 +0000

    Add search functionality

--- gui/CleanCycleGUI/app/src/main/java/uk/ac/cam/cl/cleancyclegui/MainActivity.java
+++ gui/CleanCycleGUI/app/src/main/java/uk/ac/cam/cl/cleancyclegui/MainActivity.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cl.cleancyclegui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,8 +19,24 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import uk.ac.cam.cl.cleancyclegraph.Edge;
+import uk.ac.cam.cl.cleancyclegraph.Node;
+
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private List<Polyline> polylines = new ArrayList<>();
 
     // fragment which contains the GoogleMap
     private SupportMapFragment mapFragment;
@@ -37,6 +55,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     // this activity (for use in listeners etc.)
     private MainActivity main;
+
+    private AutocompleteSupportFragment searchAutoComplete;
+
+    private PlacesClient placesClient;
+    Map<Long, Node> nodes = new HashMap<>();
+    Map<Long, List<Edge>> edges = new HashMap<>();
+
 
     /**
      * Hide the provided fragment using a fragment transaction
@@ -89,9 +114,17 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     };
 
+    private LatLng getCurrentLocation() {
+        return new LatLng(52.211472, 0.10381);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Places.initialize(getApplicationContext(), getString(R.string.google_maps_key));
+        placesClient = Places.createClient(this);
+
         setContentView(R.layout.activity_main);
 
         // START initialise class variables
@@ -104,6 +137,21 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         navigationView = (BottomNavigationView) findViewById(R.id.navigation);
         navigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
 
+        searchAutoComplete = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        searchAutoComplete.getView().setBackgroundColor(Color.WHITE);
+        searchAutoComplete.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+        searchAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                plotRoute(getCurrentLocation(), place.getLatLng());
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+
         main = this;
         // END initialise class variables
 
@@ -113,17 +161,79 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         hideFragment(notificationsFragment);
     }
 
+    private void clearMap() {
+        while (!polylines.isEmpty()) {
+            Polyline pl = polylines.get(0);
+            pl.remove();
+            polylines.remove(0);
+        }
+    }
+
     /**
      * Plot a route on the map, given a series of points
      * @param points The array of points to plot
      */
     public void plotRoute(LatLng... points) {
+        clearMap();
         Polyline line = map.addPolyline(
                 new PolylineOptions()
                     .add(points)
                 .width(5)
                 .color(0xFF005331)
         );
+        polylines.add(line);
+    }
+
+    public void plotRoute(List<Long> nodeIds) {
+        LatLng[] points = new LatLng[nodeIds.size()];
+        for (int i=0; i<nodeIds.size(); ++i) {
+            Node node = nodes.get(nodeIds.get(i));
+            points[i] = new LatLng(node.Latitude, node.Longitude);
+        }
+        plotRoute(points);
+    }
+
+    /**
+     * TODO: normalise pollution input based on maximum and minimum values (i.e. find maximum and minimum pollution values).
+     * TODO: think of a better scaling than linear as it has the effect that as pollution gets worse, areas with previously red pollution start fading to green
+     *       maybe just use thresholding?
+     * @param pollution The amount of pollution
+     * @return The rgba colour represented as an integer
+     */
+    private int getPollutionColour(double pollution) {
+        double maxPollution = 1.0;
+        double minPollution = 0.0;
+
+        double normalisedPollution = (pollution - minPollution) / (maxPollution - minPollution);
+
+        if (normalisedPollution < 0.5) {
+            float red = 1.0f;
+            float green = 2 * (float) normalisedPollution;
+            float blue = 0f;
+            return Color.valueOf(red, green, blue).toArgb();
+        } else {
+            float red = 1f - 2 * ((float) normalisedPollution - 0.5f);
+            float green = 1f;
+            float blue = 0f;
+            return Color.valueOf(red, green, blue).toArgb();
+        }
+    }
+
+    public void plotEdge(Edge edge) {
+        Node start = nodes.get(edge.Node1ID);
+        Node end = nodes.get(edge.Node2ID);
+        if (start == null || end == null) return;
+        Polyline line = map.addPolyline(
+                new PolylineOptions()
+                        .add(new LatLng(start.Latitude, start.Longitude), new LatLng(end.Latitude, end.Longitude))
+                        .width(5)
+                        .color(getPollutionColour(edge.Pollution))
+        );
+        polylines.add(line);
+    }
+
+    public void plotWay(List<Edge> way) {
+        for (Edge edge : way) plotEdge(edge);
     }
 
     /**
