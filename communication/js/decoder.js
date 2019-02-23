10025b4a167bc8011b207add33d9c6ac6120613e
Ivan <ivanmgribeiro@gmail.com>
2019-02-23 21:04:04 +0000

    Decoder now handles crossing boundaries

--- communication/js/decoder.js
+++ communication/js/decoder.js
@@ -1,5 +1,11 @@
-const SCALE = 524288;
-const OFFSET = 180;
+var SCALE = 524288;
+var OFFSET = 180;
+
+// if two datapoints differ by this much, we assume that the next-least
+// significant bit should have been increased so we need to increase
+// the latitude/longitude accordingly
+// this is 2^10 divided by the scale
+var THRESHOLD = 1024/SCALE;
 
 // returns bit in src at the specified index
 function getBit(src, index) {
@@ -11,10 +17,7 @@ function getBit(src, index) {
 // decode the input bytes and return the values as a 32 element array of numbers
 function Decoder(bytes, port) {
     // set up the return array
-    var decoded = [0, 0, 0, 0, 0, 0, 0, 0,
-                   0, 0, 0, 0, 0, 0, 0, 0,
-                   0, 0, 0, 0, 0, 0, 0, 0,
-                   0, 0, 0, 0, 0, 0, 0, 0];
+    var decoded = {};
 
     //read in base latitude and longitude
     var baseLatitude = (bytes[0]<<9) + (bytes[1]<<1) + (bytes[2]>>>7);
@@ -57,6 +60,37 @@ function Decoder(bytes, port) {
     for (var i = 0; i < 8; i++) {
         decoded[4*i] = decoded[4*i]/SCALE - OFFSET;
         decoded[4*i + 1] = decoded[4*i + 1]/SCALE - OFFSET;
+    }
+
+    // deal with potentially crossing boundaries
+    for (var i = 0; i < 7; i++) {
+        if (decoded[4*(i+1)] - decoded[4*i] > THRESHOLD) {
+            // decrease all following latitude datapoints because we
+            // crossed the south boundary
+            for (var j = i+1; j < 8; j++) {
+                decoded[4*j] -= 2*THRESHOLD;
+            }
+        } else if (decoded[4*(i+1)] - decoded[4*i] < -THRESHOLD) {
+            // increase all following latitude datapoints because we
+            // crossed the north boundary
+            for (var j = i+1; j < 8; j++) {
+                decoded[4*j] += 2*THRESHOLD;
+            }
+        }
+
+        if (decoded[4*(i+1) + 1] - decoded[4*i + 1] > THRESHOLD) {
+            // decrease all following longitude datapoints because we
+            // crossed the west boundary
+            for (var j = i+1; j < 8; j++) {
+                decoded[4*j + 1] -= 2*THRESHOLD;
+            }
+        } else if (decoded[4*(i+1) + 1] - decoded[4*i + 1] < -THRESHOLD) {
+            // increase all following longitude datapoints because we
+            // crossed the east boundary
+            for (var j = i+1; j < 8; j++) {
+                decoded[4*j + 1] += 2*THRESHOLD;
+            }
+        }
     }
 
     return decoded;
