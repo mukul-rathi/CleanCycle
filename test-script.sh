b05831621c49aa54a6c543652f19d295d37ab5a2
Mukul Rathi <mukul.rathi@gmail.com>
2019-02-24 00:26:05 +0000

    Add debugging to see why ENV variables not found in Travis environment

--- test-script.sh
+++ test-script.sh
@@ -6,7 +6,8 @@ gradle build
 if [ -f "./database-server/docker-compose.yml" ] 
     then 
     echo "POSTGRES_DB=testdb POSTGRES_USER=test 
-POSTGRES_PASSWORD=test" >> database-server/database.env
+POSTGRES_PASSWORD=test" > database-server/database.env
+    cat database-server/database.env # print out ENV variable values (for debugging)
     docker-compose -f database-server/docker-compose.yml build
     docker-compose -f database-server/docker-compose.yml up
 
